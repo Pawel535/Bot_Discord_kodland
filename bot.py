43d04f3cd09dbc2eacf98ff2983e7764af07770e
Pawel535 <132359208+Pawel535@users.noreply.github.com>
2024-10-11 17:07:23 +0200

    Update bot.py - bot o segregacji

--- bot.py
+++ bot.py
@@ -1,46 +1,57 @@
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
+import random
 
 intents = discord.Intents.default()
 intents.message_content = True
+ID_CHANNEL = 1284169325518782519
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-bot = commands.Bot(command_prefix='$', intents=intents)
-
-questions_answers = {
-    "jakie jest najpopularniejsze język programowania?": "Python",
-    "ile jest 2 + 2?": "4",
-    "jak masz na imię?": "Jestem botem i nie mam imienia :)"
+smieci_info = {
+    "niebieski": "służą do składowania papieru. Do koszów w kolorze niebieskim można wrzucać czasopisma, gazety, książki, zeszyty, papierowe torby, opakowania papierowe, kartony.",
+    "zielone": "do nich należy wrzucać szkło. Znajdziemy w nich szklane pojemniki po kosmetykach, słoiki, butelki. Pod żadnym pozorem nie wolno wrzucać do zielonych pojemników szkła żaroodpornego czy pojemników szklanych, które nierozerwalnie połączone są z innymi materiałami.",
+    "żółte": "służą do składowania metalu i innych tworzyw sztucznych. Możemy do nich wrzucać plastikowe opakowania, foliowe torebki, opakowania wielomateriałowe, takie jak kartony po mleku. Co więcej, w takim pojemniku jest miejsce na plastikowe butelki wraz z nakrętkami, kapsle, puszki, folię aluminiową czy metalowe nakrętki.",
+    "brązowe": "dedykowane są odpadom biodegradowalnym, biologicznym. W takich koszach znaleźć się powinny resztki jedzenia, ale także odpady zielone, czyli liście, kora, resztki roślin, niemalowane drewno i wiele więcej.",
+    "czarne": "służą do składowania odpadów mieszanych. Do nich trzeba wrzucać odpady skórzane, ubrania, obuwie, odchody zwierząt, zepsute naczynia, zużyte chusteczki, ręczniki papierowe, waciki, patyczki do uszu czy podpaski."
 }
+porady = [
+    "Zakręć kran podczas mycia zębów, aby oszczędzać wodę.",
+    "Używaj toreb wielokrotnego użytku zamiast plastikowych.",
+    "Zrezygnuj z jednorazowych produktów i zastąp je trwałymi alternatywami.",
+    "Zgaś światło, kiedy wychodzisz z pokoju.",
+    "Przełącz się na energooszczędne żarówki LED.",
+    "Kupuj lokalne produkty, aby zmniejszyć ślad węglowy transportu.",
+    "Oszczędzaj energię, odłączając urządzenia, gdy ich nie używasz.",
+    "Kompostuj resztki jedzenia, aby zmniejszyć ilość odpadów.",
+    "Przejedź rowerem lub chodź pieszo, zamiast korzystać z samochodu na krótkich dystansach.",
+    "Używaj bidonu zamiast kupować butelki z wodą jednorazowego użytku.",
+    "Uprawiaj rośliny w domu, aby poprawić jakość powietrza."
+]
+
+kolory = smieci_info.keys()
+
+def random_kolor():
+    return random.choice(list(kolory))
+
+@bot.command()
+async def segregacja(ctx, wybrany_kolor=''):
+    if wybrany_kolor == '':
+        wybrany_kolor = random_kolor()
+    if wybrany_kolor in smieci_info:
+        await ctx.send(f'Do koszy o kolorze {wybrany_kolor} wrzucamy: {smieci_info[wybrany_kolor]}')
+    else:
+        await ctx.send("Nieznany kolor kosza. Dostępne kolory to: niebieski, zielone, żółte, brązowe, czarne.")
+
+@tasks.loop(seconds=30)
+async def wysylaj_porady():
+    kanal = bot.get_channel(ID_CHANNEL)
+    if kanal is not None:
+        porada = random.choice(porady)
+        await kanal.send(f'Porada dotycząca pomocy planecie: {porada}')
 
 @bot.event
 async def on_ready():
-    print(f'Zalogowaliśmy się jako {bot.user}')
-
-@bot.command()
-async def hello(ctx):
-    await ctx.send(f'Cześć, jestem bot {bot.user}!')
-
-@bot.command()
-async def heh(ctx, count_heh=5):
-    await ctx.send("he" * count_heh)
-
-@bot.command()
-async def pytanie(ctx, *, question):
-    question_lower = question.lower()
-    if question_lower in questions_answers:
-        answer = questions_answers[question_lower]
-        await ctx.send(f'Odpowiedź: {answer}')
-    else:
-        await ctx.send("Nie znam odpowiedzi na to pytanie. Jak powinna brzmieć odpowiedź?")
-
-        def check(msg):
-            return msg.author == ctx.author and msg.channel == ctx.channel
-
-        try:
-            response = await bot.wait_for('message', check=check, timeout=30.0)
-            questions_answers[question_lower] = response.content
-            await ctx.send(f"Dziękuję! Zapisano odpowiedź: {response.content}")
-        except discord.errors.TimeoutError:
-            await ctx.send("Nie otrzymałem odpowiedzi. Pytanie nie zostało zapisane.")
-
-bot.run("Token")
+    print(f'Zalogowano jako {bot.user}')
+    wysylaj_porady.start()
+    
+bot.run("TOKEN")
